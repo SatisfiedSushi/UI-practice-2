568a44f26a64428b00196f801f469059476284fc
SatisfiedSushi <71732695+SatisfiedSushi@users.noreply.github.com>
2021-01-14 15:32:01 -0600

    crouch
    added croushing (lots of bugs)

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -8,6 +8,7 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 100f;
 
     public Transform playerBody;
+    public Transform gameObjectToMove;
 
     float xRotation = 0f;
     // Start is called before the first frame update
@@ -27,5 +28,15 @@ public class MouseLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKeyDown("left shift"))
+        {
+            gameObjectToMove.transform.position = new Vector3(gameObjectToMove.position.x,  gameObjectToMove.position.y - (float)0.2, gameObjectToMove.position.z);
+        }
+        
+        if (Input.GetKeyUp("left shift"))
+        {
+            gameObjectToMove.transform.position = new Vector3(gameObjectToMove.position.x, gameObjectToMove.position.y + (float)0.2, gameObjectToMove.position.z);
+        }
     }
 }
