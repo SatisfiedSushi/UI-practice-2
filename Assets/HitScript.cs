a3ddfcfa523b544fce87f208fc71769e343d8537
SatisfiedSushi <71732695+SatisfiedSushi@users.noreply.github.com>
2021-01-18 14:19:54 -0600

    added AI

---
+++ Assets/HitScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HitScript : MonoBehaviour
+{
+
+    public float health = 50f;
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
